ada4865923ea70507801cb47126c83a57c2d71bd
PACIF1ER <komaygorodsky@i.ua>
2017-04-03 23:54:07 +0300

    added items model&controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :lists
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :lists do
+  	resources :items
+  end
+  
+root 'lists#index'
 end
