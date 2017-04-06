b234ed658bc8ee505b193211c4cfb367d274c007
PACIF1ER <komaygorodsky@i.ua>
2017-04-06 19:26:29 +0300

    Some fixs v2

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,19 @@ module New
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  
+    ActionMailer::Base.smtp_settings = {
+ 	:address 			=> 'smtp.gmail.com',
+ 	:domain  			=> 'gmail.com',
+ 	:port    			=>  587,
+ 	:user_name 			=> "bydlovasya1488@gmail.com",
+ 	:password   		=> "allahuakbar1998",
+ 	:authentication		=> 'login',
+ 	:enable_starttls_auto => true
+
+
+
+
+ }
 
   end
 end
