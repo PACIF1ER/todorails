040bf2921789ebc3dc342a3f6743605c21452be6
PACIF1ER <komaygorodsky@i.ua>
2017-04-07 22:22:51 +0300

    Added bootstrap

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,7 @@ module New
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_mailer.default_url_options = { host: 'fast-sands-25972.herokuapp.com', :protocol => 'https' }
-    ActionMailer::Base.smtp_settings = {
-  :address      => 'smtp.gmail.com',
-  :domain       => 'gmail.com',
-  :port         =>  587,
-  :user_name      => "bydlovasya1488@gmail.com",
-  :password       => "allahuakbar1998",
-  :authentication   => 'plain',
-  :enable_starttls_auto => true
-    }
+   
 
   end
 end
