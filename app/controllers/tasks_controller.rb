0b72350149f049b2b8bbbaea00a1573c34a8fc16
PACIF1ER <komaygorodsky@i.ua>
2017-04-20 02:58:04 +0300

    some fixs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,8 +48,8 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: tasks_path }
       else
         format.html { render :edit }
         format.json { render json: @task.errors, status: :unprocessable_entity }
