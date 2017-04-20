82b76ba61c9d84272e210fe7e8af709328d68db6
PACIF1ER <komaygorodsky@i.ua>
2017-04-20 14:47:27 +0300

    123

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,11 @@ class Task < ApplicationRecord
 	 validates :name, presence: true,
   					length:{ minimum: 3 }
   	 validates	:priority, numericality: { only_integer: true }
+  	 validate :duedate_cannot_be_in_the_past
+  	 private
+    def duedate_cannot_be_in_the_past
+      if duedate.present? && duedate < Date.today
+        errors.add(:duedate, "Due date can't be in the past")
+      end
+    end
 end
