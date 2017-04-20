099576bcd835bdeb0dc279bced0833bfc6bef490
PACIF1ER <vbidlan@mail.ru>
2017-04-20 16:29:56 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module New
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  config.i18n.default_locale = :en
+   config.i18n.default_locale = :en
    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.gmail.com',
