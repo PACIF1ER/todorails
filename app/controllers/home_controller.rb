dc78b47059080221a93b9b520c793de3f24ed559
PACIF1ER <vbidlan@mail.ru>
2017-04-20 16:24:49 +0300

    Fixs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
   	if user_signed_in?
-			redirect_to tasks_path
+	  redirect_to tasks_path
 	else
-		redirect_to new_user_registration_url 
+	   redirect_to new_user_registration_url 
 	end
   end
 end
